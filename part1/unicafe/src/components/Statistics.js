e8484e0c66bf36f915b44cbf2c1fa4f82a200634
sheceido <leonpoon@hotmail.ca>
2022-10-04 00:12:29 -0400

    Commit#1.1: part1 unicafe edits.

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,35 +1,36 @@
 import StatisticLine from './StatisticLine';
 
 const Statistics = ({good, neutral, bad}) => {
-
-    if (good !== 0 || neutral !== 0 || bad !== 0) {
-      const total = good + neutral + bad;
-      
-      let average = ((good - bad) / (good + neutral + bad));
-      average = Math.round(average * 10) / 10;
-      
-      let positive = (good / (good + neutral + bad));
-      positive = Math.round(positive * 1000) / 10;
-      
-      return (
-        <>
-        <h2>Statistics</h2>
-          <table>
-                <tbody>
-                    <StatisticLine text="Good" value={good}/>
-                    <StatisticLine text="Neutral" value ={neutral}/>
-                    <StatisticLine text="Bad" value={bad}/>
-                    <StatisticLine text="all" value={total}/>
-                    <StatisticLine text="average" value={average}/>
-                    <StatisticLine text="positive" value={positive + '%'}/>
-                </tbody>
-          </table>
-
-        </>
-      );
-    }
   
-    return (<p>No feedback given</p>);
-  }
+  const total = good + neutral + bad;
+    
+  if (total !== 0) {
+    
+    let average = ((good - bad) / total);
+    average = Math.round(average * 10) / 10;
+    
+    let positive = (good / total);
+    positive = Math.round(positive * 1000) / 10;
+    
+    return (
+      <>
+      <h2>Statistics</h2>
+        <table>
+              <tbody>
+                  <StatisticLine text="Good" value={good}/>
+                  <StatisticLine text="Neutral" value ={neutral}/>
+                  <StatisticLine text="Bad" value={bad}/>
+                  <StatisticLine text="all" value={total}/>
+                  <StatisticLine text="average" value={average}/>
+                  <StatisticLine text="positive" value={positive + '%'}/>
+              </tbody>
+        </table>
 
-  export default Statistics;
+      </>
+    );
+  }
+  
+  return (<p>No feedback given</p>);
+}
+
+export default Statistics;
